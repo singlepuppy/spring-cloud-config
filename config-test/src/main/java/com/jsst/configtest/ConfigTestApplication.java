81dd72ab084567e5f7a0acc70cf8febb92d85b59
WangGJ <guangjian.wang@jieshunpay.cn>
2018-07-27 14:41:13 +0800

    change config

--- config-test/src/main/java/com/jsst/configtest/ConfigTestApplication.java
+++ config-test/src/main/java/com/jsst/configtest/ConfigTestApplication.java
@@ -1,15 +1,14 @@
-package com.jsst.configserver;
+package com.jsst.configtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class ConfigTestApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigTestApplication.class, args);
     }
 }
